34b6d99b5ec6007e24c1efb157b67e8540830bf5
haicheniadmytro <haichenia.dmytro@sportlife.kiev.ua>
2026-08-23 19:44:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
